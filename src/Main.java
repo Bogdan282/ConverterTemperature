a97b4347ef1f5b20afb4b0be31693cbd6391c77b
Bogdan282 <bogdan673876@gmail.com>
2025-04-09 08:38:16 +0300

    Add Celsius convert to Fahrenheit

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,16 @@ public class Main {
         double fahrenheit = 15, celsius = 20;
         double convertedFahrenheit = fahrToCels(fahrenheit);
         System.out.printf("%n%.1f фарегейтів, це %.1f цельсіїв.%n", fahrenheit, convertedFahrenheit);
+
+        double convertedCelsius = celsToFahr(celsius);
+        System.out.printf("%.1f цельсіїв, це %.1f фарангейтів.%n", celsius, convertedCelsius);
     }
     public static double fahrToCels(double fahrenheit) {
         double Cels = ((fahrenheit - 32) / 1.8);
         return Cels;
+    }
+    public static double celsToFahr(double celsius) {
+        double Fahr = (celsius * 9/5) + 32;
+        return Fahr;
     }
 }
